34088faee9da80e1a6c2ef3f7183f2e57d6388f9
Martin Drlík <martindrlik@hotmail.com>
2019-09-10 03:55:11 +0200

    clipboard

--- sdl/clipboard.go
+++ sdl/clipboard.go
@@ -8,8 +8,8 @@ import (
 	"unsafe"
 )
 
-// Text gets UTF-8 text from the clipboard.
-func Text() (string, error) {
+// ClipboardText gets UTF-8 text from the clipboard.
+func ClipboardText() (string, error) {
 	cs := C.SDL_GetClipboardText()
 	if cs == nil {
 		return "", errors.New(Error())
@@ -19,14 +19,14 @@ func Text() (string, error) {
 	return s, nil
 }
 
-// HasText gets a flag indicating whether the clipboard
+// HasClipboardText gets a flag indicating whether the clipboard
 // exists and contains a text string that is non-empty.
-func HasText() bool {
+func HasClipboardText() bool {
 	return C.SDL_HasClipboardText() == C.SDL_TRUE
 }
 
-// SetText puts UTF-8 text into the clipboard.
-func SetText(text string) error {
+// SetClipboardText puts UTF-8 text into the clipboard.
+func SetClipboardText(text string) error {
 	ct := C.CString(text)
 	if C.SDL_SetClipboardText(ct) < 0 {
 		return errors.New(Error())
